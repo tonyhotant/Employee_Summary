c4f77698283e54f015b2b982a45903f924f62613
Lisheng <lishengyu1986@gmail.com>
2020-04-28 19:58:26 +0800

    fix class issue, all tests passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,15 +1,15 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-  constructor(name, id, email, role, github) {
-    super(name, id, email, role);
+  constructor(name, id, email, github) {
+    super(name, id, email, "Engineer");
     this.github = github;
   }
   getRole() {
-      return this.role;
+    return this.role;
   }
   getGithub() {
-      return this.github;
+    return this.github;
   }
 }
 module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,8 +1,8 @@
 const Employee = require("./Employee");
 
 class Manager extends Employee {
-  constructor(name, id, email, role, officeNumber) {
-    super(name, id, email, role);
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email, "Manager");
     this.officeNumber = officeNumber;
   }
   getRole() {
